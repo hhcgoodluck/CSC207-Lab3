10ae3173d492ef6364631d2d090447ce8ed6f5fc
Hongcheng Huo <hongcheng.huo@mail.utoronto.ca>
2024-09-30 05:25:03 -0400

    Work Done

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-// TODO CheckStyle: Wrong lexicographical order for 'java.util.HashMap' import (remove this comment once resolved)
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class provides the service of converting country codes to their names.
  */
 public class CountryCodeConverter {
-
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    // pick appropriate instance variable(s) to store the data necessary for this class
+    private Map<String, String> countryCodeMap;
 
     /**
      * Default constructor which will load the country codes from "country-codes.txt"
@@ -30,38 +31,52 @@ public class CountryCodeConverter {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public CountryCodeConverter(String filename) {
-
+        // 初始化 HashMap
+        countryCodeMap = new HashMap<>();
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
-
-            // TODO Task: use lines to populate the instance variable(s)
-
+            String regex = "^(.*?)\\s+[A-Z]{2}\\s+([A-Z]{3})\\s+\\d+$";
+            Pattern pattern = Pattern.compile(regex);
+            // use lines to populate the instance variable(s)
+            for (String line : lines.subList(1, lines.size())) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    String countryName = matcher.group(1).trim();
+                    String alpha3Code = matcher.group(2);
+                    // 实现 Map alpha3code --> countryName
+                    countryCodeMap.put(alpha3Code, countryName);
+                }
+            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
-
     }
 
     /**
      * Returns the name of the country for the given country code.
-     * @param code the 3-letter code of the country
+     * @param code the 3-letter code of the country                输入3位code 返回国家名称
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return code;
+        // update this code to use an instance variable to return the correct value
+        return countryCodeMap.getOrDefault(code.toUpperCase(), "Country code not found");
     }
 
     /**
      * Returns the code of the country for the given country name.
-     * @param country the name of the country
+     * @param country the name of the country                      输入国家名称 返回3位code
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return country;
+        // update this code to use an instance variable to return the correct value
+        for (Map.Entry<String, String> entry : countryCodeMap.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase(country)) {
+                return entry.getKey();
+            }
+        }
+        return "Country not found";
     }
 
     /**
@@ -69,7 +84,7 @@ public class CountryCodeConverter {
      * @return how many countries are included in this code converter.
      */
     public int getNumCountries() {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return 0;
+        // update this code to use an instance variable to return the correct value
+        return countryCodeMap.size();
     }
 }
--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,17 +5,21 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
  * data from a JSON file. The data is read in once each time an instance of this class is constructed.
  */
 public class JSONTranslator implements Translator {
-
-    // TODO Task: pick appropriate instance variables for this class
+    // pick appropriate instance variables for this class
+    // 存储 Map 键: alpha3 code -- 键值：2位code
+    private Map<String, List<Map<String, String>>> countryLanguagesMap;
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -30,16 +34,32 @@ public class JSONTranslator implements Translator {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public JSONTranslator(String filename) {
+        countryLanguagesMap = new HashMap<>();
+
         // read the file to get the data to populate things...
         try {
-
             String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource(filename).toURI()));
-
             JSONArray jsonArray = new JSONArray(jsonString);
+            // use the data in the jsonArray to populate your instance variables
+            // Note: this will likely be one of the most substantial pieces of code you write in this lab.
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            // 使用 jsonArray 中的数据填充实例变量
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject countryObject = jsonArray.getJSONObject(i);
+                String alpha3 = countryObject.getString("alpha3");
+                // 创建某个 alpha3 对应的语言代码及其翻译映射的列表
+                List<Map<String, String>> languages = new ArrayList<>();
 
+                // 收集该国家的所有语言代码
+                for (String langKey : countryObject.keySet()) {
+                    if (!"id".equals(langKey) && !"alpha3".equals(langKey) && !"alpha2".equals(langKey)) {
+                        Map<String, String> langMap = new HashMap<>();
+                        langMap.put(langKey, countryObject.getString(langKey));
+                        languages.add(langMap);
+                    }
+                }
+                countryLanguagesMap.put(alpha3, languages);
+            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -48,21 +68,46 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        // return an appropriate list of language codes, but make sure there is no aliasing to a mutable object
+        // 根据给定国家的 alpha3 代码获取对应的语言代码列表
+        List<Map<String, String>> languagesMaps = countryLanguagesMap.get(country);
+        // 创建一个新的列表以保存语言代码
+        List<String> languageCodes = new ArrayList<>();
+        // 检查国家是否存在，且确保没有对可变对象的别名
+        if (languagesMaps != null) {
+            for (Map<String, String> langMap : languagesMaps) {
+                // 将每个语言映射的键（语言代码）添加到列表中
+                languageCodes.addAll(langMap.keySet());
+            }
+        }
+        // 返回一个新的列表以避免对可变对象的别名
+        return new ArrayList<>(languageCodes);
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        // return an appropriate list of country codes, but make sure there is no aliasing to a mutable object
+        // 使用 keySet() 方法获取国家代码的集合，并返回一个新的列表以避免可变对象的别名
+        return new ArrayList<>(countryLanguagesMap.keySet());
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
-        return null;
+        // complete this method using your instance variables as needed
+        // 根据给定的国家代码（country）和语言代码（language），返回该国家在指定语言下的翻译文本
+        // 获取与给定国家 alpha3 代码相关联的语言翻译信息列表
+        List<Map<String, String>> languageMaps = countryLanguagesMap.get(country);
+        // 检查国家是否存在，并且语言翻译列表不为空
+        if (languageMaps != null) {
+            // 遍历每个语言映射，查找所需的语言代码
+            for (Map<String, String> languageMap : languageMaps) {
+                if (languageMap.containsKey(language)) {
+                    // 返回对应的翻译文本
+                    return languageMap.get(language);
+                }
+            }
+        }
+        // 如果未找到对应的翻译，返回自定义消息
+        return "Translation not available for " + country + " in " + language;
     }
 }
